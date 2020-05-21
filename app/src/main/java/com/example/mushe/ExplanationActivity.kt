7d8d4357df3a0134ef64991444f1760a8adc4886
qwsd1109 <37693678+qwsd1109@users.noreply.github.com>
2020-05-21 17:10:42 +0900

    [add] 화면간의 전환

--- app/src/main/java/com/example/mushe/ExplanationActivity.kt
+++ app/src/main/java/com/example/mushe/ExplanationActivity.kt
@@ -1,12 +1,22 @@
 package com.example.mushe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_explanation.*
 
 class ExplanationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_explanation)
+
+        btn_signup.setOnClickListener {
+            startActivity(Intent(this,SignUpActivity::class.java))
+        }
+
+        btn_signin.setOnClickListener {
+            startActivity(Intent(this,SignInActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/example/mushe/SignInActivity.kt
+++ app/src/main/java/com/example/mushe/SignInActivity.kt
@@ -1,12 +1,23 @@
 package com.example.mushe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_sign_in.*
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignInActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        tv_sign_up.setOnClickListener {
+            startActivity(Intent(this,SignUpActivity::class.java))
+        }
+        tv_missing_pw.setOnClickListener {
+            Toast.makeText(this,"고객센터에 문의해주시길 바람니다.",Toast.LENGTH_SHORT).show()
+        }
     }
 }
